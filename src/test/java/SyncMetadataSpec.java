b7f4bb78e74d6a23fe95502862ede488acffb627
Nirmal <nirmal.akliparambil@gmail.com>
2016-11-07 12:06:16 +0530

    initial commit

---
+++ src/test/java/SyncMetadataSpec.java
@@ -0,0 +1,51 @@
+import static org.junit.Assert.*;
+
+import java.util.Optional;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class SyncMetadataSpec {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void syncFileCanBeLoadedFromClasspath() {
+		SyncMetadata.load("sync-settings.txt");
+	}
+
+	@Test
+	public void sourceConfigIsPresent() {
+		SyncMetadata metadata = SyncMetadata.load("sync-settings.txt");
+		assertTrue(metadata.isPresent("/home/nirmal/Documents"));
+	}
+	
+	@Test
+	public void sourceConfigIsAbsent() {
+		SyncMetadata metadata = SyncMetadata.load("sync-settings.txt");
+		assertFalse(metadata.isPresent("/home/nirmal"));
+	}
+	
+	@Test
+	public void getDestinationForADirectSource() {
+		SyncMetadata metadata = SyncMetadata.load("sync-settings.txt");
+		Optional<String> destination = metadata.getDestination("/home/nirmal/Documents");
+		assertTrue("Destination mapping for '/home/nirmal/Documents' should be present", destination.isPresent());
+		assertEquals("/media/hdd-ntfs/nirmal/pc/Documents", destination.get());
+	}
+	
+	@Test
+	public void getDestinationForAnIndirectSource() {
+		SyncMetadata metadata = SyncMetadata.load("sync-settings.txt");
+		Optional<String> destination = metadata.getDestination("/home/nirmal/Documents/Official");
+		assertTrue("Destination mapping for '/home/nirmal/Documents/Official' should be present", destination.isPresent());
+		assertEquals("/media/hdd-ntfs/nirmal", destination.get());
+	}
+}
